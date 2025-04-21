84e5e2d6564227c53b94ce59698280fd3e24805f
Ishan Pranav <ishan.pranav@outlook.com>
2025-04-21 11:36:06 -0400

    Create view

--- create.sql
+++ create.sql
@@ -1,31 +1,31 @@
 -- create.sql
 -- Licensed under the MIT license.
 
-DROP TABLE IF EXISTS athlete_event CASCADE;
-DROP TABLE IF EXISTS noc_region;
+drop table if exists athlete_event CASCADE;
+drop table if exists noc_region;
 
-CREATE TABLE noc_region (
+create table noc_region (
 	noc varchar(10) primary key, 
 	region text,
 	note text
 );
 
-CREATE TABLE athlete_event (
+create table athlete_event (
     athlete_event_id serial primary key,
 	id integer, 
-	name text,
+	"name" text,
 	sex varchar(1),  
 	age integer,
 	height numeric,
-	weight numeric,
+	"weight" numeric,
 	team text, 
 	noc varchar(10),
 	games text, 
-	year integer, 
+	"year" integer, 
 	season varchar(10),
 	city text,
 	sport text,
-	event text,
+	"event" text,
 	medal text
 );
 
@@ -34,7 +34,7 @@ from "data/noc_regions.csv"
 with csv null as 'NA' header
 ;
 
-copy athlete_event (id, name, sex, age, height, weight, team, noc, games, year, season, city, sport, event, medal)
+copy athlete_event (id, "name", sex, age, height, "weight", team, noc, games, "year", season, city, sport, "event", medal)
 from "data/athlete_events.csv"
 with csv null as 'NA' header
 ;
--- queries.sql
+++ queries.sql
@@ -1,1 +1,30 @@
--- TODO: write the task number and description followed by the query
+-- queries.sql
+-- Licensed under the MIT license.
+
+-- 1. Write a view
+--    This view automatically joins the two tables based on `noc`, only shows
+--    rows where a medal exists, and groups to count medals.
+
+create or replace view medal_summary as
+select
+    athlete_event.athlete_event_id as athlete_event_id,
+    athlete_event."name",
+    athlete_event.sex,
+    athlete_event.age,
+    athlete_event.height,
+    athlete_event."weight",
+    athlete_event.team,
+    athlete_event.noc,
+    noc_region.region,
+    noc_region.note,
+    athlete_event.games,
+    athlete_event."year",
+    athlete_event.season,
+    athlete_event.city,
+    athlete_event.sport,
+    athlete_event."event",
+    athlete_event.medal
+from athlete_event
+join noc_region on athlete_event.noc = noc_region.noc
+where athlete_event.medal is not null
+;
