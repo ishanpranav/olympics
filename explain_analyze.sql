9c4da4c33b72915b953b3744efdfade54adbb865
Ishan Pranav <ishan.pranav@outlook.com>
2025-04-21 23:28:01 -0400

    Part 3.5

--- explain_analyze.sql
+++ explain_analyze.sql
@@ -8,26 +8,39 @@ drop index if exists athlete_event_name_idx;
 -- 2. Write a simple query:
 
 select * from athlete_event
-where "name" ilike '%Michael Fred Phelps, II%'
+where "name" = 'Michael Fred Phelps, II'
 ;
 
 -- 3. Use `EXPLAIN ANALYZE`:
 
 explain analyze
 select * from athlete_event
-where "name" ilike '%Michael Fred Phelps, II%'
+where "name" = 'Michael Fred Phelps, II'
 ;
 
---  Gather  (cost=1000.00..8215.76 rows=27 width=137) (actual time=58.644..89.019 rows=30 loops=1)
+--   Gather  (cost=1000.00..8213.36 rows=3 width=137) (actual time=10.807..20.110 rows=30 loops=1)
 --    Workers Planned: 2
 --    Workers Launched: 2
---    ->  Parallel Seq Scan on athlete_event  (cost=0.00..7213.06 rows=11 width=137) (actual time=62.524..79.036 rows=10 loops=3)
---          Filter: (name ~~* '%Michael Fred Phelps, II%'::text)
+--    ->  Parallel Seq Scan on athlete_event  (cost=0.00..7213.06 rows=1 width=137) (actual time=6.904..8.628 rows=10 loops=3)
+--          Filter: (name = 'Michael Fred Phelps, II'::text)
 --          Rows Removed by Filter: 90362
---  Planning Time: 0.226 ms
---  Execution Time: 89.180 ms
+--  Planning Time: 0.321 ms
+--  Execution Time: 20.138 ms
 -- (8 rows)
 
 -- 4. Add an index:
 
 create index athlete_event_name_idx on athlete_event("name");
+
+-- 5. Verifying improved performance:
+
+explain analyze
+select * from athlete_event
+where "name" = 'Michael Fred Phelps, II'
+;
+
+--  Index Scan using athlete_event_name_idx on athlete_event  (cost=0.42..16.42 rows=3 width=137) (actual time=0.227..0.232 rows=30 loops=1)
+--    Index Cond: (name = 'Michael Fred Phelps, II'::text)
+--  Planning Time: 1.375 ms
+--  Execution Time: 0.330 ms
+-- (4 rows)
