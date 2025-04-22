6cab0191c8ffff430cd57a942d77ee0bf43402e6
Ishan Pranav <ishan.pranav@outlook.com>
2025-04-21 23:21:14 -0400

    Part 3.1

--- explain_analyze.sql
+++ explain_analyze.sql
@@ -1,1 +1,8 @@
--- TODO: use explain / analyze, create an index
+-- explain_analyze.sql
+-- Licensed under the MIT license.
+
+-- 1. Drop an existing index:
+
+drop index if exists athlete_event_name_idx;
+
+-- 2. 
